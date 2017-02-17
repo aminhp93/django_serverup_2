2fd9b6f61e752f5529d52b0d92859aef9a8bd96b
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-17 09:36:15 -0800

    11 - Setup Views

--- src/videos/views.py
+++ src/videos/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import (
+		CreateView,
+		DetailView,
+		ListView,
+		UpdateView,
+		DeleteView,
+	)
 
+from .models import Video
 # Create your views here.
+
+class VideoCreateView(CreateView):
+	queryset = Video.objects.all()
+
+class VideoDetailView(DetailView):
+	queryset = Video.objects.all()
+
+class VideoListView(ListView):
+	queryset = Video.objects.all()
+
+class VideoUpdateView(UpdateView):
+	queryset = Video.objects.all()
+
+class VideoDeleteView(DeleteView):
+	queryset = Video.objects.all()
