9c018c1b95b4390c39305e4b8a51c2297b02c643
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-17 09:19:44 -0800

    10 - Customizing Admin

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -3,4 +3,16 @@ from django.contrib import admin
 from .models import Video
 # Register your models here.
 
-admin.site.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+	list_filter = ['title', 'timestamp']
+	list_display = ['title', 'updated', 'timestamp']
+	readonly_fields = ['updated', 'timestamp', 'short_title']
+	search_fields = ['title', 'embed_code']
+	
+	class Meta:
+		model = Video
+
+	def short_title(self, obj):
+		return obj.title[:3]
+
+admin.site.register(Video, VideoAdmin)
--- src/videos/models.py
+++ src/videos/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Video(models.Model):
 	embed_code 	= models.TextField()
 	title 		= models.CharField(max_length=120)
+	updated 	= models.DateTimeField(auto_now=True)
 	timestamp 	= models.DateTimeField(auto_now_add=True)
 
 
