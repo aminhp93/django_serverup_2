27c6c2c74d4bfc2c9270ecec7013338be4389417
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-18 14:32:00 -0800

    36 - Add lecture to admin

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, Lecture
 # Register your models here.
 
-admin.site.register(Course)
+class LectureInline(admin.TabularInline):
+	model = Lecture
+
+class CourseAdmin(admin.ModelAdmin):
+	inlines = [LectureInline]
+	list_filter = ['title', 'timestamp']
+	list_display = ['title', 'updated', 'timestamp']
+	readonly_fields = ['updated', 'timestamp']
+	search_fields = ['title', 'description']
+
+	class Meta:
+		model = Course
+
+
+admin.site.register(Course, CourseAdmin)
