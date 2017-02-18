6dbca02dbbade24036ca46f4aa8157478a8dbcdb
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-17 20:26:17 -0800

    27 - Course Setup Part 1

---
+++ src/courses/views.py
@@ -0,0 +1,53 @@
+import random
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import (
+		CreateView,
+		DetailView,
+		ListView,
+		UpdateView,
+		DeleteView,
+	)
+
+from .models import Course
+from .mixins import MemberRequiredMixin, StaffMemberRequiredMixin
+# from .forms import VideoForm
+
+# Create your views here.
+
+class CourseCreateView(LoginRequiredMixin, CreateView):
+	model = Course 
+	form_class = CourseForm
+	# success_url = "/"
+
+class CourseDetailView(StaffMemberRequiredMixin, DetailView):
+	queryset = Course.objects.all()
+
+	def get_object(self):
+		abc = self.kwargs.get("slug")
+		return get_object_or_404(Course, slug=abc)
+
+	def get_context_data(self, *args, **kwargs):
+		context = super().get_context_data(*args, **kwargs)
+		return context
+
+class CourseListView(ListView):
+
+	def get_queryset(self):
+		request = self.request
+		qs = Course.objects.all()
+		query = request.GET.get('q')
+		if query:
+			qs = qs.filter(title__icontains=query)
+		return qs#filter(title__icontains="em")#.filter(user=self.request.user)
+
+	def get_context_data(self, *args, **kwargs):
+		context = super().get_context_data(*args, **kwargs)
+		context["random_number"] = random.randint(0,10)
+		return context
+
+class CourseUpdateView(UpdateView):
+	queryset = Course.objects.all()
+
+class CourseDeleteView(DeleteView):
+	queryset = Course.objects.all()
