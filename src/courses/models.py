6eacc0eb064201e4c2283422929fc2280bb35d3f
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-18 14:18:35 -0800

    35 - Lecture Model

--- src/courses/models.py
+++ src/courses/models.py
@@ -4,6 +4,8 @@ from django.db.models.signals import pre_save, post_save
 from django.utils.text import slugify
 from django.core.urlresolvers import reverse
 
+from videos.models import Video
+
 from .utils import create_slug
 
 # Create your models here.
@@ -22,9 +24,24 @@ class Course(models.Model):
 	def get_absolute_url(self):
 		return reverse("courses:detail", kwargs={"slug": self.slug})
 
+class Lecture(models.Model):
+	course 			= models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+	video 			= models.ForeignKey(Video, on_delete=models.SET_NULL, null=True) 
+	title 		    = models.CharField(max_length=120)
+	slug 			= models.SlugField(blank=True) # unique=True
+	description 	= models.TextField()
+	updated 		= models.DateTimeField(auto_now=True)
+	timestamp 		= models.DateTimeField(auto_now_add=True) 
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse("courses:detail", kwargs={"slug": self.course.slug})
 
 def pre_save_video_receiver(sender, instance, *args, **kwargs):
 	if not instance.slug:
 		instance.slug = create_slug(instance)
 
 pre_save.connect(pre_save_video_receiver, sender=Course)
+pre_save.connect(pre_save_video_receiver, sender=Lecture)
