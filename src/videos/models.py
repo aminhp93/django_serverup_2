641f56488d0a6ec6b4058b98e9147717c4911209
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-17 16:21:22 -0800

    19 - Auto Generate Slugs with Signals

--- src/videos/models.py
+++ src/videos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.utils.text import slugify
 
 # Create your models here.
 class Video(models.Model):
@@ -8,6 +10,19 @@ class Video(models.Model):
 	updated 	= models.DateTimeField(auto_now=True)
 	timestamp 	= models.DateTimeField(auto_now_add=True)
 
-
 	def __str__(self):
 		return self.title
+
+def pre_save_video_receiver(sender, instance, *args, **kwargs):
+	if not instance.slug:
+		instance.slug = slugify(instance.title)
+
+pre_save.connect(pre_save_video_receiver, sender=Video)
+
+# def post_save_video_receiver(sender, instance, created, *args, **kwargs):
+# 	if not instance.slug:
+# 		instance.slug = slugify(instance.title)
+# 		instance.save()
+
+# post_save.connect(post_save_video_receiver, sender=Video)
+
