9ec1e2553657ed74b528931023e9c1a0a7ee6825
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-25 09:56:42 -0800

    55 - Override Category Context Data

--- src/categories/views.py
+++ src/categories/views.py
@@ -16,3 +16,18 @@ class CategoryListView(ListView):
 
 class CategoryDetailView(DetailView):
 	queryset = Category.objects.all()
+
+	def get_context_data(self, *args, **kwargs):
+		context = super().get_context_data(*args, **kwargs)
+		# obj = self.get_object()
+		obj = context.get("object")
+		print(context, obj)
+		user = self.request.user
+		qs1 = obj.primary_category.all()
+		qs2 = obj.secondary_category.all()
+		if user.is_authenticated():
+			qs1 = qs1.owned(self.request.user)
+			qs2 = qs2.owned(self.request.user)
+		qs = (qs1 | qs2).distinct()
+		context['courses'] = qs
+		return context
